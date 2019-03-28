6a5185d456950202eb7b762df1ba8d9b120c2986
Artsapollo <lemart03@gmail.com>
2019-03-28 19:17:48 +0200

    Update

--- src/main/java/springData/dto/OfficeDetails.java
+++ src/main/java/springData/dto/OfficeDetails.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class OfficeDetails {
     @Max(value = 50000, message = "4")
@@ -30,5 +31,27 @@ public class OfficeDetails {
 
     public void setSales(BigDecimal sales) {
         this.sales = sales;
+    }
+
+    @Override
+    public String toString() {
+        return "OfficeDetails{" +
+                "target=" + target +
+                ", sales=" + sales +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OfficeDetails that = (OfficeDetails) o;
+        return Objects.equals(target, that.target) &&
+                Objects.equals(sales, that.sales);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(target, sales);
     }
 }
